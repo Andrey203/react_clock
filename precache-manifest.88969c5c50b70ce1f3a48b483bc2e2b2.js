dbe80a835bdeeb923b9b7fe383e4f6a190b4a3c9
Andrey Dorohov <andrey.dorohov8@gmail.com>
2021-05-20 15:46:47 +0300

    Updates

---
+++ precache-manifest.88969c5c50b70ce1f3a48b483bc2e2b2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe6ad2c4dc8629e52d4a163ce381874d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "7891c33d84fd2f535079",
+    "url": "./static/css/main.819e4dd6.chunk.css"
+  },
+  {
+    "revision": "9bfabb54970d8a1edebf",
+    "url": "./static/js/2.c17dec48.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.c17dec48.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7891c33d84fd2f535079",
+    "url": "./static/js/main.de170ee1.chunk.js"
+  },
+  {
+    "revision": "89de80f3985b25002b5f",
+    "url": "./static/js/runtime-main.17b9eccd.js"
+  }
+]);
